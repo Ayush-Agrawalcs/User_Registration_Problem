088e59b75073e4355d4f0fe706b21c3abfefe4f7
Ayush Agrawal <agrawal07ayushbansal@gmail.com>
2026-03-19 12:37:52 +0530

    Validate password minimum 8 character

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import re
 class User:
     '''
     Add valid User'''
-    def __init__(self,First_name,Last_name,email,phone_no):
+    def __init__(self,First_name,Last_name,email,phone_no,password):
         self.First_name=First_name
         self.Last_name=Last_name
         self.email=email
         self.phone_no=phone_no
+        self.password=password
     
     def __str__(self):
-        return f"{self.First_name} {self.Last_name} {self.email} {self.phone_no}"
+        return f"{self.First_name} {self.Last_name} {self.email} {self.phone_no} {self.password}"
     
 
 def isvalid_first_name(First_name):
@@ -66,22 +67,38 @@ def isvalid_Phone_no(phone_no):
             print(e)
 
 
+def isvalid_password(password):
+        '''
+        Validate password minimum 8 character
+        '''
+        try:
+            pattern=r"^[a-zA-Z0-9]{8,}"
+            mat=re.match(pattern,password)
+            if not mat:
+                raise ValueError("Invalid password")
+            return True
+        except ValueError as e:
+            print(e)
+
      
 
     
 
 def main():
     while(True):
-        First_name=input("Enter the first name:")
-        Last_name=input("Enter the Last name:")
-        email=input("Enter the email:")
-        phone_no=input("Enter the phone no.")
+        First_name=input("Enter the first name: ")
+        Last_name=input("Enter the Last name: ")
+        email=input("Enter the email: ")
+        phone_no=input("Enter the phone no.: ")
+        password=input("Enter the password: ")
         valid_first=isvalid_first_name(First_name)
         valid_Last=isvalid_Last_name(Last_name)
         valid_email=isvalid_email(email)
         valid_phone_no=isvalid_Phone_no(phone_no)
-        if(valid_first==True and valid_Last==True and valid_email==True and valid_phone_no==True ) :
-            user=User(First_name,Last_name,email,phone_no)
+        valid_password=isvalid_password(password)
+
+        if(valid_first==True and valid_Last==True and valid_email==True and valid_phone_no==True and valid_password==True) :
+            user=User(First_name,Last_name,email,phone_no,password)
             print(user)
             break
 
